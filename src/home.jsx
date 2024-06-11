17e6eaa0e256439452462c91f462a8c0ec98277e
devteam0099 <av.dev2024@gmail.com>
2024-06-11 18:10:35 +0500

    temperory modifications on testing base

--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,40 @@
-function Home() {
-    return(
-        <><h1>this is Home  page</h1>
-        </>
-    )
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+export default function Home() {
+  let [image,setimage] = useState(null)
+  let [image2,setimage2] = useState(null)
+  let [image3,setimage3] = useState(null)
+  let [text,settext] = useState("")
+  
+    
+    async function submithandler(e){
+        e.preventDefault()
+        let formdata = new FormData()
+        formdata.append('username', text)
+        formdata.append('images', image)
+        formdata.append('images',image2)
+        formdata.append('images',image3)
+
+        
+       try{
+        alert("submitted")
+       await axios.post('http://localhost:3000/',formdata)
+       }
+       catch{
+        alert("err")
+       }
+    }
+  return (
+    <div>
+      <form encType='multi-part/formdata' method='Post'>
+      <input type='text' onChange={(e)=>{settext(e.target.value)}} className='border-[black] border-2' />
+      <input type='file' onChange={(e)=>{setimage(e.target.files[0])}} />
+      <input type='file' onChange={(e)=>{setimage2(e.target.files[0])}} />
+      <input type='file' onChange={(e)=>{setimage3(e.target.files[0])}} />
+      <button onClick={submithandler}>submit</button>
+      </form>
+    </div>
+  )
 }
-export default Home
--- src/login.jsx
+++ src/login.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import sample7 from './images/sample7.jpg'
 import sample8 from './images/sample8.jpg'
 import sample1 from './images/sample1.avif'
+import axios from 'axios'
 function Login(){
     let [shuffleform,setshufflevalue] = useState(true);
+    let [image,setimage]= useState()
     let [inputfield,setinputfield] = useState({
         firstname : "",
         lastname : "",
@@ -13,11 +15,36 @@ function Login(){
         confpassword : "",
 
     })
+    console.log(inputfield)
     function formshuffle(){
         setshufflevalue(!shuffleform);
     }
     function inputchangehandler(inputfieldno,inputvalue){
         setinputfield((prev_value)=>({...prev_value, [inputfieldno] : inputvalue.target.value}))
+    }
+    function postdata(){
+       
+        const {firstname,lastname,email,username,password} = inputfield
+        
+        async function backend(inputfield){
+            console.log(inputfield)
+        await fetch('/api/login', {
+             method: 'post',
+             body: JSON.stringify(firstname,lastname,email,username,password),
+             headers: {
+               'Content-Type': 'application/json'
+             }
+           })
+           .then((resp) => { alert("data is successfully sent to server")
+            
+            })
+           .catch(() => { alert("error sending data") })
+           console.log("after sending to server" + firstname,lastname,email,username,password)
+          console.log( typeof(firstname,lastname,email,username,password))
+          
+    } backend()}
+    function imagehandler(e){
+         setimage(e.target.files[0])
     }
     return(
         < >
@@ -42,8 +69,8 @@ function Login(){
             <h3 className="text-center font-bold text-xl my-[3%] text-orange-500">Confirm Password</h3>
             <input type="password" required={true} value={inputfield.confpassword} className="border-orange-500 border-2 mx-[20%] w-[60%] h-[40px] rounded text-center " placeholder="confirm password" onChange={(e)=>inputchangehandler('confpassword',e)} />
             <h3 className="text-center font-bold text-xl my-[3%] text-orange-500">Enter Profile Image</h3>
-            <input type="file" className="border-orange-500 border-2 mx-[20%] w-[60%] h-[40px] rounded"   />
-            <button className="text-white bg-orange-500 rounded px-5 py-2 mx-[40%] my-3 border-orange-500 border-2  ">Submit</button>
+            <input type="file" className="border-orange-500 border-2 mx-[20%] w-[60%] h-[40px] rounded" onChange={imagehandler}  />
+            <button className="text-white bg-orange-500 rounded px-5 py-2 mx-[40%] my-3 border-orange-500 border-2  " onClick={postdata} >Submit</button>
             <h6 className="mx-[25%] text-white">Already have an account?<span onClick={formshuffle} className="text-black text-orange-500 font-bold cursor-pointer">Login</span></h6>
            
              </div>
